a63e065686ddcd3aa734153bb636f98a0347eae2
mosDigital <mosquito.zhang@gmail.com>
2019-01-30 13:19:05 +0800

    wrap in a class

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ziwenLog
-Configured logging for future script...Python.
+# myLogConfig
+Configured logging for future python script.
+
+Loggers are never instantiated directly, but always through the module-level function logging.getLogger(name). Multiple calls to getLogger() with the same name will always return a reference to the same Logger object.
+
+Even though, I prefer to wrap the config into a class.
+
+
+#how to use:
+import logging
+from LogConfigPkg import myLogConfig
+
+class A:
+    def __init__(self):
+        self.logger=logging.getLogger(__name__)
+        self.logger.debug('class a init')
+
+if __name__=='__main__':
+    myLogConfig=myLogConfig.ConfigMyLog(logFileName='test')
+    logger=logging.getLogger(__name__)
+    logger.info('start main')
+    tmpA=A()
+
---
+++ myLogConfig.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import time
+
+
+class ConfigMyLog():
+
+    @property
+    def logfile(self):
+        return self._logFile
+
+    @property
+    def logfilepath(self):
+        return self._logFilePath
+
+    @property
+    def logger(self):
+        return self._logger
+
+
+    def __init__(self,logFileName='default',withFolder=True,consoleLevel=logging.DEBUG,logLevel=logging.DEBUG):
+        if withFolder:
+            self._logFilePath=r'./'+str(time.strftime("%Y%m%d%H%M%S", time.localtime()))
+            if not os.path.exists(self._logFilePath):
+                os.makedirs(self._logFilePath)
+            self._logFile=os.path.abspath(self._logFilePath+r'/'+logFileName+r'.log')
+        else:
+            self._logFilePath=r'./'
+            self._logFile=os.path.abspath(r'./'+logFileName+r'.log')
+        self.__setupDefaultLevel(consoleLevel,logLevel)
+
+
+    def __setupDefaultLevel(self,consoleLevel,logLevel):
+        #setup console.
+        console=logging.StreamHandler()
+        console.setLevel(consoleLevel)
+        formatterConsole = logging.Formatter('%(name)-20s: %(levelname)-8s %(message)s')
+        console.setFormatter(formatterConsole)
+        #setup default
+        filehandler = logging.FileHandler(self._logFile,mode='w',encoding='utf8',delay=False)
+        logging.basicConfig(level=logLevel,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s: %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                handlers=[filehandler,console])
